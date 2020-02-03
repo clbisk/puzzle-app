ae316c1211a8307123796fc3e49fa643def7b333
Cecilia Bisk <clbisk@gmail.com>
2020-02-03 17:41:27 -0500

    added headers to see what's what

--- src/app/puzzle.service.ts
+++ src/app/puzzle.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PuzzleService {
   PUZZLE_URL = "https://wbeo70qxe2.execute-api.us-east-1.amazonaws.com/Prod/"
+  headers: HttpHeaders
 
   constructor(private http: HttpClient) { }
 
   getPuzzle(puzzleType: string, id: string) {
-    return this.http.get<Puzzle>(this.PUZZLE_URL + puzzleType + "/" + id);
+    this.headers = new HttpHeaders().append('Access-Control-Allow-Origin', '*').append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS').append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    return this.http.get<Puzzle>(this.PUZZLE_URL + puzzleType + "/" + id, {"headers": this.headers});
   }
 
   putRatings(puzzleType: string, id: string, rating: number, difficultyRating: number) {
@@ -24,3 +26,4 @@ export interface Puzzle {
   solution: string;
   ratings: Array<number>;
 }
+
